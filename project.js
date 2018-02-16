a7c6ab374ce42f5c234289eb90a1f579f53df218
Seray Uzgur <serayuzgur@gmail.com>
2018-02-16 22:12:44 +0300

    a build bug fixed

--- project.js
+++ project.js
@@ -1,3 +1,7 @@
 module.exports = require("marko-starter").projectConfig({
-  name: "marko-example" // Optional, but added here for demo purposes
+  name: "marko-example", // Optional, but added here for demo purposes
+  minify: true,
+  minifyCss: true,
+  minifyJs: true,
+  fingerPrintsEnabled: true,
 });
